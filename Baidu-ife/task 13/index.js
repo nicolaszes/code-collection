7b5a912a6840b2a7bf48765c05600ea6f3bf95bb
nicolaszs <1316236724@qq.com>
2017-02-16 19:47:23 +0800

    baidu-ife/task 13, 事件冒泡？

--- Baidu-ife/task 13/index.js
+++ Baidu-ife/task 13/index.js
@@ -1,28 +1,28 @@
 function DisplayAirData() {
-	let aqiInput = document.getElementById("aqi-input");
-	let	button = document.getElementById("button");
-	let	aqiDisplay = document.getElementById("aqi-display");
-	
-	// 获取aqi-input输入的值，并显示在aqi-display中
-	function updateDisplay() {
-		//判断输入的内容
-		if (aqiInput.value.trim().match(/^[0-9]+$/)) {
-			aqiDisplay.innerText = aqiInput.value;
-		} else {
-			alert ("请输入正确的数字!");
-			aqiDisplay.innerText = "输入错误!";
-		}
-	}
+  	let aqiInput = document.getElementById("aqi-input");
+  	let	button = document.getElementById("button");
+  	let	aqiDisplay = document.getElementById("aqi-display");
+  	
+  	// 获取aqi-input输入的值，并显示在aqi-display中
+  	function updateDisplay() {
+    		//判断输入的内容
+    		if (aqiInput.value.trim().match(/^[0-9]+$/)) {
+    			  aqiDisplay.innerText = aqiInput.value;
+    		} else {
+    			  alert ("请输入正确的数字!");
+    			  aqiDisplay.innerText = "输入错误!";
+    		}
+  	}
 
-	//给按钮button绑定一个点击事件
-	button.onclick = updateDisplay();
-	
-	// enter键入
-	aqiInput.onkeydown = function(evt) {
-		if (evt.keyCode === 13) {
-			updateDisplay();
-		}
-	};
+  	//给按钮button绑定一个点击事件
+  	button.onclick = updateDisplay;
+  	
+  	// enter键入
+  	aqiInput.onkeydown = function(evt) {
+  		  if (evt.keyCode === 13) {
+  			    updateDisplay();
+  		  }
+  	};
 };
 
 DisplayAirData();
