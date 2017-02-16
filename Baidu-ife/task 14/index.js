85aa3404bd3c903358e62c5822fcaa9f348e8e67
nicolaszs <1316236724@qq.com>
2017-02-16 19:58:13 +0800

    nothing

--- Baidu-ife/task 14/index.js
+++ Baidu-ife/task 14/index.js
@@ -10,14 +10,14 @@ let aqiData = [
 (function () {
    
     //先进行过滤，选出空气质量指数大于60的城市
-    let newData = aqiData.filter(item => {
+    let newData = aqiData.filter( item => {
         return (item[1] > 60);
     });
 
     //对指数大于60的城市进行降序排序
     newData.sort( (a, b) => {
         return b[1] - a[1];
-    });
+    })
 
     //将结果显示
     let aqiList = document.getElementById('aqi-list');
