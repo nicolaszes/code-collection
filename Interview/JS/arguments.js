11eb45dc487bd4bd20c773188cdfaa216d3cfd9b
nicolaszs <1316236724@qq.com>
2017-12-16 21:39:40 +0800

    add argu

--- Interview/JS/arguments.js
+++ Interview/JS/arguments.js
@@ -52,3 +52,65 @@ Array.prototype.slice.call(arrayLike);
 Array.prototype.concat.apply([], arrayLike);
 // 4. ES6 Array.from
 Array.from(arrayLike);
+
+
+/**
+ * arguments object
+ */
+function foo(name, age, sex) {
+  console.log(arguments);
+}
+
+foo('name', 'age', 'sex')
+
+/**
+ * length 属性
+ * Arguments对象的length属性，表示实参的长度，举个例子
+ */
+function foo(b, c, d){
+  console.log("实参的长度为：" + arguments.length)
+}
+console.log("形参的长度为：" + foo.length)
+foo(1)
+// 形参的长度为：3
+// 实参的长度为：1
+
+/**
+ * callee属性
+ * Arguments 对象的 callee 属性，通过它可以调用函数自身。
+ * 讲个闭包经典面试题使用 callee 的解决方法
+ */
+var data = [];
+for (var i = 0; i < 3; i++) {
+  (data[i] = function () {
+    console.log(arguments.callee.i) 
+  }).i = i;
+}
+
+data[0]();
+data[1]();
+data[2]();
+// 0
+// 1
+// 2
+
+
+/**
+ * arguments 和对应参数的绑定
+ */
+function foo(name, age, sex, hobbit) {
+  console.log(name, arguments[0]); // name name
+  // 改变形参
+  name = 'new name';
+  console.log(name, arguments[0]); // new name new name
+  // 改变arguments
+  arguments[1] = 'new age';
+  console.log(age, arguments[1]); // new age new age
+  // 测试未传入的是否会绑定
+  console.log(sex); // undefined
+  sex = 'new sex';
+  console.log(sex, arguments[2]); // new sex undefined
+  arguments[3] = 'new hobbit';
+  console.log(hobbit, arguments[3]); // undefined new hobbit
+}
+foo('name', 'age')
