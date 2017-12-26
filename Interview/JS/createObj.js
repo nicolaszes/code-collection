d44c59a787717af3b6afb9246c672cf84f666dbd
nicolaszs <1316236724@qq.com>
2017-12-26 17:32:38 +0800

    js create obj

---
+++ Interview/JS/createObj.js
@@ -0,0 +1,123 @@
+/**
+ * 1.工厂模式
+ */
+function createPerson (name) {
+  var o = new Object();
+  o.name = name;
+  o.getName = function () {
+    console.log(this.name)
+  }
+
+  return o;
+}
+
+var person1 = createPerson('nico')
+// 对象无法识别，因为所有的实例都指向一个原型
+
+/**
+ * 2.构造函数模式
+ */
+function Person (name) {
+  this.name = name;
+  this.getName = function () {
+    console.log(this.name)
+  }
+}
+
+var person2 = new Person('name')
+// 优点：实例可以识别为y一个特定的类型
+// 缺点：每次创建实例时，每个方法都要被创建一次
+
+/**
+ * 2-1.构造函数模式优化
+ */
+function Person (name) {
+  this.name = name;
+  this.getName = getName
+}
+function getName() {
+  console.log(this.name);
+}
+var person1 = new Person('nico');
+// 解决了每个方法都要被重新创建的问题
+// 没有封装
+
+/**
+ * 3.原型模式
+ */
+function Person (name) {}
+Person.prototype.name = name;
+Person.prototype.getName = function () {
+  console.log(this.name);
+};
+
+var person1 = new Person();
+// 优点：方法不会重新创建
+// 1.所有的属性和方法都共享，2.不能初始化参数
+
+/**
+ * 3.1 原型模式优化
+ */
+function Person (name) {};
+Person.prototype = {
+  name: 'nico',
+  getName: function () {
+    console.log(this.name)
+  }
+}
+
+var person1 = new Person();
+// 优点：封装性较好
+// 缺点：重写了原型，丢失了 constructor属性
+
+/**
+ * 3.2 原型模式优化
+ */
+function Person (name) {};
+Person.prototype = {
+  constructor: Person,
+  name: 'nico',
+  getName: function () {
+    console.log(this.name);
+  }
+}
+
+var person1 = new Person();
+// 优点：实例可以通过 constructor属性找到所属的构造函数
+// 缺点：原型模式该有的缺点还是有
+
+
+/**
+ * 4.组合模式
+ */
+function Person (name) {
+  this.name = name;
+}
+Person.prototype = {
+  constructor: Person,
+  getname: function () {
+    console.log(this.name)
+  }
+}
+
+var person1 = new Person();
+// 优点：该共享的共享，该私有的私有
+// 缺点：有的人就是希望全部写在一起，即封装性
+
+/**
+ * 4.1 动态原型模式
+ */
+function Person(name) {
+  this.name = name;
+  if (typeof this.getName !== 'function') {
+    Person.prototype = {
+      constructor: Person,
+      getName: function () {
+        console.log(this.name);
+      }
+    }
+  }
+}
+
+var person1 = new Person('nico');
+var person2 = new Person('coco');
