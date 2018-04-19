8add4b7b839b7b051ec7e11c5c8a3789d09d51e9
nicolaszs <1316236724@qq.com>
2018-04-19 14:58:20 +0800

    add mediator

--- Interview/DesignPattern/mediator/index.js
+++ Interview/DesignPattern/mediator/index.js
@@ -194,3 +194,139 @@ player2.die()
 player3.die()
 player4.die()
 
+/**
+ * 中介者模式购买商品
+ */
+var goods = {   // 手机库存
+  "red": 3,
+  "blue": 6
+};
+
+colorSelect.onchange = function(){
+  var color = this.value,   // 颜色
+      number = numberInput.value,   // 数量
+      stock = goods[ color ]   // 该颜色手机对应的当前库存
+
+  colorInfo.innerHTML = color
+
+  if ( !color ){
+      nextBtn.disabled = true
+      nextBtn.innerHTML = '请选择手机颜色'
+      return
+  }
+
+  if ( ( ( number - 0 ) | 0 ) !== number - 0 ){   // 用户输入的购买数量是否为正整数
+      nextBtn.disabled = true
+      nextBtn.innerHTML = '请输入正确的购买数量'
+      return
+  }
+
+  if ( number > stock  ){   // 当前选择数量超过库存量
+    nextBtn.disabled = true
+    nextBtn.innerHTML = '库存不足'
+    return 
+  }
+
+  nextBtn.disabled = false
+  nextBtn.innerHTML = '放入购物车'
+}
+
+numberInput.oninput = function(){
+  var color = colorSelect.value,   // 颜色
+      number = this.value,   // 数量
+      stock = goods[ color ]  // 该颜色手机对应的当前库存
+
+  numberInfo.innerHTML = number
+
+  if ( !color ){
+      nextBtn.disabled = true
+      nextBtn.innerHTML = '请选择手机颜色'
+      return;
+  }
+
+  if ( ( ( number - 0 ) | 0 ) !== number - 0 ){   // 输入购买数量是否为正整数
+      nextBtn.disabled = true
+      nextBtn.innerHTML = '请输入正确的购买数量'
+      return;
+  }
+
+  if ( number > stock  ){   // 当前选择数量没有超过库存量
+      nextBtn.disabled = true
+      nextBtn.innerHTML = '库存不足'
+      return
+  }
+
+  nextBtn.disabled = false
+  nextBtn.innerHTML = '放入购物车'
+}
+
+/**
+ * 可能遇到的困难
+ * 假设现在就要求去掉 colorInfo和 numberInfo这两个展示区域
+ * 我们需要分别改动 colorSelect.onchange 和 numberInput.oninput
+ * 因为先前的代码，对象耦合在了一起
+ * 
+ * 如果页面的节点激增到 10 - 15个，它们之间的联系将变得错综复杂
+ * 
+ * 引入中介者
+ * 新增一些跟需求相关的节点，只需要修改中介者函数
+ * 中介者模式是迎合迪米特法则的一种实现。迪米特法则也叫最少知识原则，是指一个对象应该尽可能少地了解另外的对象
+ */
+var mediator = (function () {
+  var colorSelect = document.getElementById( 'colorSelect' ),
+      memorySelect = document.getElementById( 'memorySelect' ),
+      numberInput = document.getElementById( 'numberInput' ),
+      colorInfo = document.getElementById( 'colorInfo' ),
+      memoryInfo = document.getElementById( 'memoryInfo' ),
+      numberInfo = document.getElementById( 'numberInfo' ),
+      nextBtn = document.getElementById( 'nextBtn' );
+
+  return {
+    changed: function( obj ){
+      var color = colorSelect.value,   // 颜色
+          memory = memorySelect.value,// 内存
+          number = numberInput.value,   // 数量
+          stock = goods[ color + '|' + memory ]  // 颜色和内存对应的手机库存数量
+  
+      if ( obj === colorSelect ){     // 如果改变的是选择颜色下拉框
+          colorInfo.innerHTML = color
+      }else if ( obj === memorySelect ){
+          memoryInfo.innerHTML = memory
+      }else if ( obj === numberInput ){
+          numberInfo.innerHTML = number
+      }
+  
+      if ( !color ){
+          nextBtn.disabled = true
+          nextBtn.innerHTML = '请选择手机颜色'
+          return
+      }
+  
+      if ( !memory ){
+          nextBtn.disabled = true
+          nextBtn.innerHTML = '请选择内存大小'
+          return
+      }
+  
+      if ( ( ( number - 0 ) | 0 ) !== number - 0 ){   // 输入购买数量是否为正整数
+          nextBtn.disabled = true
+          nextBtn.innerHTML = '请输入正确的购买数量'
+          return
+      }
+  
+      nextBtn.disabled = false
+      nextBtn.innerHTML = '放入购物车'
+    }
+  }
+})()
+
+// 事件函数：
+colorSelect.onchange = function(){
+  mediator.changed( this );
+}
+memorySelect.onchange = function(){
+  mediator.changed( this );
+}
+numberInput.oninput = function(){
+  mediator.changed( this );
+}
