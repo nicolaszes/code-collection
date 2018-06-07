36b9d7661e1b05b54d7a675d48918dad3ca77319
nicolaszs <1316236724@qq.com>
2018-06-07 08:54:42 +0800

    add axis in chart

--- d3v4/src/app.js
+++ d3v4/src/app.js
@@ -52,56 +52,108 @@
 // d3.select('.action').remove()
 // console.log(secondLink.nodes())
 
-const scores = [
-  { name: 'Alice', score: 96 },
-  { name: 'Billy', score: 83 },
-  { name: 'Cindy', score: 91 },
-  { name: 'David', score: 96 },
-  { name: 'Emily', score: 88 },
-]
+// const scores = [
+//   { name: 'Alice', score: 96 },
+//   { name: 'Billy', score: 83 },
+//   { name: 'Cindy', score: 91 },
+//   { name: 'David', score: 96 },
+//   { name: 'Emily', score: 88 },
+// ]
 
-function scaleBar (selection, scale) {
-  selection.style('transform', `scaleX(${scale})`)
+// function scaleBar (selection, scale) {
+//   selection.style('transform', `scaleX(${scale})`)
+// }
+
+// function fade (selection, opacity) {
+//   selection.style('fill-opacity', opacity)
+// }
+
+// const bar = d3.select('.chart')
+//   .append('svg')
+//     .attr('width', 225)
+//     .attr('height', 300)
+//   .selectAll('rect')
+//   .data(scores, function (d) {
+//     return d ? d.name : this.innerText
+//   })
+//   .style('color', 'blue')
+//   .enter()
+//     .append('g')
+//     .attr('transform', (d, i) => `translate(0, ${i * 33})`)
+
+// bar.append('rect')
+//     .text((d) => d.name)
+//     .attr('class', 'bar')
+//     .style('width', d => d.score)
+//     .on('mouseover', function (d, i, elements) {
+//       d3.select(this).call(scaleBar, 2)
+//       // console.log(d3.selectAll(elements)
+//       // .filter(':not(:hover)'))
+//       d3.selectAll(elements)
+//         .filter(':not(:hover)')
+//         .style('fill-opacity', .5)
+//         // .classed('barOn', true)
+//     })
+//     .on('mouseout', function (d, i, elements) {
+//       d3.selectAll(elements)
+//         .style('fill-opacity', 1)
+//       d3.select(this).classed('barOn', false)
+//       d3.select(this).call(scaleBar, 1)
+//     })
+
+// bar.append('text')
+//   .text(d => d.name)
+//   .attr('y', 20)
+//   .attr('x', 10)
+
+const margin = {
+  top: 25,
+  right: 25,
+  bottom: 40,
+  left: 40,
 }
+const width = 425 - margin.left - margin.right
+const height = 625 - margin.top - margin.bottom
 
-function fade (selection, opacity) {
-  selection.style('fill-opacity', opacity)
-}
-
-const bar = d3.select('.chart')
+const svg = d3.select('.chart')
   .append('svg')
-    .attr('width', 225)
-    .attr('height', 300)
-  .selectAll('rect')
-  .data(scores, function (d) {
-    return d ? d.name : this.innerText
-  })
-  .style('color', 'blue')
-  .enter()
-    .append('g')
-    .attr('transform', (d, i) => `translate(0, ${i * 33})`)
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-bar.append('rect')
-    .text((d) => d.name)
-    .attr('class', 'bar')
-    .style('width', d => d.score)
-    .on('mouseover', function (d, i, elements) {
-      d3.select(this).call(scaleBar, 2)
-      // console.log(d3.selectAll(elements)
-      // .filter(':not(:hover)'))
-      d3.selectAll(elements)
-        .filter(':not(:hover)')
-        .style('fill-opacity', .5)
-        // .classed('barOn', true)
-    })
-    .on('mouseout', function (d, i, elements) {
-      d3.selectAll(elements)
-        .style('fill-opacity', 1)
-      d3.select(this).classed('barOn', false)
-      d3.select(this).call(scaleBar, 1)
-    })
+svg.append('rect')
+  .attr('width', width)
+  .attr('height', height)
+  .style('fill', 'lightblue')
+  .style('stroke', 'green')
 
-bar.append('text')
-  .text(d => d.name)
-  .attr('y', 20)
-  .attr('x', 10)
+// svg.append('rect')
+//   .attr('x', width / 2)
+//   .attr('width', width / 2)
+//   .attr('height', height)
+//   .style('fill', 'lightblue')
+//   .style('stroke', 'green')
+
+const yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0])
+
+const yAxis = d3.axisLeft(yScale)
+  // .ticks(5, 's')
+  .tickValues([8, 19, 43, 77])
+svg.call(yAxis)
+
+const xScale = d3.scaleTime()
+  .domain([new Date(2016, 0, 1, 6), new Date(2016, 0, 1, 9)])
+  .range([0, width])
+
+const xAxis = d3.axisBottom(xScale)
+  // .ticks(d3.timeMinute.every(45))
+  .ticks(5)
+  .tickSizeInner(10)
+  .tickSizeOuter(20)
+  .tickPadding(15)
+svg.append('g')
+  .attr('transform', `translate(0, ${height})`)
+  .call(xAxis)
