96e5b2dc7a292b163d43548c06b1c3e14c159c8d
nicolaszs <1316236724@qq.com>
2018-09-17 11:00:04 +0800

    making ajax request

--- NodeApp/public/app.js
+++ NodeApp/public/app.js
@@ -3,6 +3,84 @@
  *
  */
 
- var app = {};
+const app = {}
 
- console.log("Hello Console World!!");
+// Config
+app.config = {
+  'sessionToken' : false
+}
+
+// AJAX Client (for RESTful API)
+app.client = {}
+
+// Interface for making API calls
+app.client.request = (headers, path, method, queryStringObject, payload, callback) => {
+  // Set defaults
+  headers = typeof(headers) == 'object' && headers !== null 
+    ? headers 
+    : {}
+  path = typeof(path) == 'string' ? path : '/'
+  method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method.toUpperCase()) > -1 
+    ? method.toUpperCase() 
+    : 'GET'
+  queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null 
+    ? queryStringObject 
+    : {}
+  payload = typeof(payload) == 'object' && payload !== null ? payload : {}
+  callback = typeof(callback) == 'function' ? callback : false
+
+  // For each query string parameter sent, add it to the path
+  const requestUrl = path + '?'
+  let counter = 0
+  for (let queryKey in queryStringObject) {
+    if (queryStringObject.hasOwnProperty(queryKey)) {
+      counter++
+      // If at least one query string parameter has already been added, preprend new ones with an ampersand
+      if (counter > 1) {
+        requestUrl += '&'
+      }
+
+      requestUrl += queryKey + '=' + queryStringObject[queryKey]
+    }
+  }
+
+  const xhr = new XMLHttpRequest()
+  xhr.open(method, requestUrl, true)
+  xhr.setRequestHeader("Content-Type", "application/json")
+
+  // For each header sent, add it to the request
+  for(let headerKey in headers){
+    if(headers.hasOwnProperty(headerKey)){
+      xhr.setRequestHeader(headerKey, headers[headerKey])
+    }
+  }
+
+  // If there is a current session token set, add that as a header
+  if(app.config.sessionToken){
+    xhr.setRequestHeader("token", app.config.sessionToken.id)
+  }
+
+  // when the request send back, handle the response
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4) {
+      const { 
+        status: statusCode,
+        responseText: responseReturned
+      } = xhr
+
+      // Callback if requested
+      if(callback){
+        try{
+          var parsedResponse = JSON.parse(responseReturned)
+          callback(statusCode, parsedResponse)
+        } catch(e){
+          callback(statusCode, false)
+        }
+      }
+    }
+  }
+
+  // Send the payload as JSON
+  let payloadString = JSON.stringify(payload)
+  xhr.send(payloadString)
+}
