5ad0bc0aad9f28910580b521f346b6be19f51dc9
nicolaszs <1316236724@qq.com>
2018-09-17 11:27:36 +0800

    create account

--- NodeApp/lib/handlers.js
+++ NodeApp/lib/handlers.js
@@ -40,6 +40,37 @@ handlers.index = (data, callback) => {
   }
 };
 
+// Create Account
+handlers.accountCreate = function(data,callback){
+  // Reject any request that isn't a GET
+  if(data.method == 'get'){
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title' : 'Create an Account',
+      'head.description' : 'Signup is easy and only takes a few seconds.',
+      'body.class' : 'accountCreate'
+    };
+    // Read in a template as a string
+    helpers.getTemplate('accountCreate',templateData,function(err,str){
+      if(!err && str){
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str,templateData,function(err,str){
+          if(!err && str){
+            // Return that page as HTML
+            callback(200,str,'html');
+          } else {
+            callback(500,undefined,'html');
+          }
+        });
+      } else {
+        callback(500,undefined,'html');
+      }
+    });
+  } else {
+    callback(405,undefined,'html');
+  }
+};
+
 // users
 handlers.users = (data, callback) => {
   const acceptableMethods = ['post', 'get', 'put', 'delete']
