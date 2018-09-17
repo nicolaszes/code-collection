e3e7735ed8cb809b71c0ddabe9c40f0c51d2abe4
nicolaszs <1316236724@qq.com>
2018-09-17 10:13:49 +0800

    add parsing request method

--- NodeApp/index.js
+++ NodeApp/index.js
@@ -5,10 +5,29 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
  // Configure the server to respond to all requests with a string
 var server = http.createServer(function(req,res){
+
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
+
+  // Send the response
   res.end('Hello World!\n');
+
+  // Log the request/response
+  console.log('Request received on path: '+trimmedPath+' with method: '+method+' and this query string: ',queryStringObject);
 });
 
 // Start the server
