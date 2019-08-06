f795f29e9d14be0f9509852f253a5876db812c4a
nicolaszs <1316236724@qq.com>
2019-08-06 22:01:31 +0800

    add promise excute example

--- Code-Fragment/Promise/example.js
+++ Code-Fragment/Promise/example.js
@@ -1,3 +1,22 @@
+// promise构造函数是同步执行的，then方法是异步执行的
+const promise = new Promise((resolve, reject) => {
+  console.log(1)
+  resolve(5)
+  console.log(2)
+}).then(val => {
+  console.log(val)
+})
+
+promise.then(() => {
+  console.log(3)
+})
+
+console.log(4)
+
+setTimeout(function () {
+  console.log(6)
+})
+
 let promiseA = new Promise((resolve, reject) => reject(42))
 let promiseB = new Promise((resolve, reject) => resolve(21))
 let promiseC = new Promise((resolve, reject) => reject(10))
@@ -13,14 +32,14 @@ let promiseC = new Promise((resolve, reject) => reject(10))
 //   console.log(res)
 // })
 
-function add (xPromise, yPromise) {
+function add(xPromise, yPromise) {
   return Promise.all([
-   xPromise,
-   yPromise,
- ]).then(res => {
-   console.log('return')
-   // return res[0] + res[1]
- })
+    xPromise,
+    yPromise,
+  ]).then(res => {
+    console.log('return')
+    // return res[0] + res[1]
+  })
 }
 
 // add(promiseA, promiseB).then(res => {
@@ -84,27 +103,27 @@ p.then(e, b).then(c, f).catch(err => {
 /**
  * async await promise 的执行顺序
  */
-async function async1(){
+async function async1() {
   console.log('async1 start')
   await async2()
   console.log('async1 end')
 }
 
-async function async2(){
+async function async2() {
   console.log('async2')
 }
 console.log('script start')
 
-setTimeout(function(){
+setTimeout(function () {
   console.log('setTimeout')
 }, 0)
 
 async1()
 
-new Promise(function(resolve){
+new Promise(function (resolve) {
   console.log('promise1')
   resolve()
-}).then(function(){
+}).then(function () {
   console.log('promise2')
 })
 console.log('script end')
