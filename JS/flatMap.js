f90f99e9630419d14cfcf0de3032ad58a87a5680
nicolaszs <1316236724@qq.com>
2019-09-16 14:36:47 +0800

    add several flatten deep arr

--- JS/flatMap.js
+++ JS/flatMap.js
@@ -2,16 +2,57 @@
 [1, [2], 3].flatMap((v) => v + 1)
 // -> [2, 3, 4]
 
-const flattenDeep = (arr) => Array.isArray(arr) ?
+/**
+ * 1.trick
+ */
+arr.toString().split(',')
+arr.join(',').split(',')
+
+/**
+ * 2.RegExp
+ */
+const flattenDeepOne = (arr) => JSON.parse('[' + JSON.stringify(arr).replace(/\[|\]/g, '') + ']')
+
+/**
+ * 3.flat(Infinity)
+ */
+const flattenDeep = arr.flat(Infinity)
+
+/**
+ * 4.recursion
+ */
+const flattenDeepTwo = (arr) => Array.isArray(arr) ?
   arr.reduce((a, b) => [...a, ...flattenDeep(b)], []) : [arr]
-
-const flattenDeep2 = arr.flat(Infinity)
-
-const flattenDeep3 = (arr) => {
+ 
+/**
+ * 5.迭代
+ */ 
+const flattenDeepThree = (arr) => {
   while (arr.some(item => Array.isArray(item))) {
     arr = [].concat(...arr)
   }
   return arr
+}
+
+/**
+ * 6.Generator
+ */
+function* flat(arr) {
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      yield* flat(item); // Generator委托
+    } else {
+      yield item
+    }
+  }
+}
+
+const flattenDeepFourth = (arr) => {
+  let result = [];
+  for (let val of (flat(arr))) {
+    result.push(val);
+  }
+  return result;
 }
 
 console.log(flatten(arr))
