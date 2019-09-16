ea106b5fd6a42f3ad3b281a0154d6b3dabb540fb
nicolaszs <1316236724@qq.com>
2019-09-16 14:14:30 +0800

    add flatten deep

---
+++ JS/Symbol.toPrimitive.js
@@ -0,0 +1,21 @@
+// 一个没有提供 Symbol.toPrimitive 属性的对象，参与运算时的输出结果
+var obj1 = {};
+console.log(+obj1);     // NaN
+console.log(`${obj1}`); // "[object Object]"
+console.log(obj1 + ""); // "[object Object]"
+
+// 接下面声明一个对象，手动赋予了 Symbol.toPrimitive 属性，再来查看输出结果
+var obj2 = {
+  [Symbol.toPrimitive](hint) {
+    if (hint == "number") {
+      return 10;
+    }
+    if (hint == "string") {
+      return "hello";
+    }
+    return true;
+  }
+};
+console.log(+obj2);     // 10      -- hint 参数值是 "number"
+console.log(`${obj2}`); // "hello" -- hint 参数值是 "string"
+console.log(obj2 + ""); // "true"  -- hint 参数值是 "default"
--- JS/flatMap.js
+++ JS/flatMap.js
@@ -2,10 +2,21 @@
 [1, [2], 3].flatMap((v) => v + 1)
 // -> [2, 3, 4]
 
-const flattenDeep = (arr) => Array.isArray(arr)
-  ? arr.reduce((a, b) => [...a, ...flattenDeep(b)] , [])
-  : [arr]
+const flattenDeep = (arr) => Array.isArray(arr) ?
+  arr.reduce((a, b) => [...a, ...flattenDeep(b)], []) : [arr]
 
 const flattenDeep2 = arr.flat(Infinity)
 
-flattenDeep([1, [[2], [3, [4]], 5]])
+const flattenDeep3 = (arr) => {
+  while (arr.some(item => Array.isArray(item))) {
+    arr = [].concat(...arr)
+  }
+  return arr
+}
+
+console.log(flatten(arr))
+
+flattenDeep([1, [
+  [2],
+  [3, [4]], 5
+]])
